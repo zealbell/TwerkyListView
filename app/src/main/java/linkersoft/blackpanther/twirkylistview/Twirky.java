aa00a91f7f8344304948466768c1ac06585fd547
LiNKeR <am.usa.to.be@gmail.com>
2018-07-15 01:47:46 +0100

    Initial commit

---
+++ app/src/main/java/linkersoft/blackpanther/twirkylistview/Twirky.java
@@ -0,0 +1,101 @@
+package linkersoft.blackpanther.twirkylistview;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import linkersoft.blackpanther.trump.TrumpyRecycler;
+import linkersoft.blackpanther.twirky.TwerkyListView;
+
+public class Twirky extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.twirky);
+        Context context=this;
+
+        //FAKE-DATA
+        ArrayList<String> data=new ArrayList<>();
+        for (int i = 0; i < 20; i++)data.add(null);
+        //TWERKING-VIEW
+        TwerkyListView twerkList=(TwerkyListView)findViewById(R.id.twerk);
+        //LAYOUT
+        TrumpyRecycler.TrumpyLinearLayoutManager tllm= new TrumpyRecycler.TrumpyLinearLayoutManager(context);
+        tllm.setOrientation(LinearLayoutManager.VERTICAL);
+        twerkList.setLayoutManager(tllm);
+        twerkList.setFlingFactor(TrumpyRecycler.MODERATE_FLING);
+        twerkList.setFrictionFactor(TrumpyRecycler.NO_FRICTION);
+        twerkList.setOverScrollMode(TrumpyRecycler.OVER_SCROLL_NEVER);
+        //ADAPTER
+        TwerkingOffAdapter twerkdapter=new TwerkingOffAdapter(context,R.layout.twerkrow,data);
+        twerkList.setAdapter(twerkdapter);
+        //TWERKING
+        twerkList.setTwerking(true);
+    }
+
+    //TWERK-ADAPTER
+    public static class TwerkingOffAdapter extends  TwerkyListView.TwerkAdapter{
+
+        private static final int TWERK_COLORS[]={  0xFFA8A8A8,0xFFBDCBB7,0xFFA3BDBF,0xFFF3F2E6,0xFFBECEC2,0xFFF4E8F7,0xFFC3D9D9, 0xFF4E4F4F,0xFFFAC8CA,0xFFEAC5B5,0xFFAEE8D5,0xFFE3DDC1,0xFFD6EFFF,0xFFF1F1CD,0xFFC7C7CF,0xFFF1DCCE };
+        ArrayList<String> data;
+        private LayoutInflater inflater;
+        private int LayoutResId;
+
+        public TwerkingOffAdapter(Context context, int LayResId, ArrayList<String> data){
+            this.data=data;
+            inflater =LayoutInflater.from(context);
+            this.LayoutResId=LayResId;
+        }
+
+        @Override
+        public int getItemCount(){
+            return (data ==null)?0: data.size();
+        }
+
+        @Override
+        public Twerkie onCreateViewHolder(ViewGroup parent, int ViewType) {
+            View row= inflater.inflate(LayoutResId,parent,false);
+            return new Twerkie(row);
+        }
+
+        @Override
+        public void onBindViewHolder(TwerkHolder twerkholder, int position){
+            Twerkie twerkie=(Twerkie)twerkholder;
+            twerkie.twerkparent.setBackgroundColor(TWERK_COLORS[getRandom(0,TWERK_COLORS.length-1)]);
+            twerkie.twerktext.setText("item "+position);
+        }
+
+
+        @Override
+        public void onViewAttachedToWindow(TwerkHolder twerkholder) {
+            super.onViewAttachedToWindow(twerkholder);/*do not forget this|it's essential for twerking*/
+        }
+
+        public class Twerkie extends  TwerkHolder {
+
+            TextView twerktext;
+            View twerkparent;
+            private Twerkie(View v){
+                super(v);
+                twerkparent= v.findViewById(R.id.twerkparent);
+                twerktext= v.findViewById(R.id.twerktext);
+            }
+        }
+        private static int getRandom(int min, int max) {
+            Random rand = new Random();
+            return rand.nextInt((max - min) + 1) + min;
+        }
+
+    }
+
+}
---
+++ twirky/src/main/java/linkersoft/blackpanther/twirky/TwerkyListView.java
@@ -0,0 +1,429 @@
+package linkersoft.blackpanther.twirky;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+
+import linkersoft.blackpanther.trump.TrumpyRecycler;
+
+
+public class TwerkyListView extends TrumpyRecycler {
+
+    public TwerkyListView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public TwerkyListView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        super.setAdapter(adapter);
+        if(!(adapter instanceof TwerkAdapter))throw new IllegalTwerkingException();
+    }
+    @Override
+    protected void in(final Context context, AttributeSet attrs) {
+        super.in(context, attrs);
+        final float stretchScale;
+        final float shrinkScale;
+
+        TypedArray trump_attrs = context.getTheme().obtainStyledAttributes(attrs, R.styleable.TwerkyListView, 0, 0);
+        try {
+            stretchScale = trump_attrs.getFloat(R.styleable.TwerkyListView_stretchScale, 0.0F);
+            shrinkScale = trump_attrs.getFloat(R.styleable.TwerkyListView_shrinkScale, 0.0F);
+            ShrinkStretchSpan = trump_attrs.getInt(R.styleable.TwerkyListView_ShrinkStretchSpan, 0);
+            int twerkStyle =  trump_attrs.getInt(R.styleable.TwerkyListView_twerkStyle, 0);
+            switch (twerkStyle){
+                case 0:
+                    upTwerk=true;
+                    style=new TwerkStyle() {
+                        @Override
+                        public void OnTwerk(MotionEvent event) {
+                            switch (event.getAction() & event.getActionMasked()){
+                                case MotionEvent.ACTION_DOWN:
+                                    iYy=(int)event.getY();
+                                    ufYy =iYy-maxYgap;
+                                    center = getCentrallyVisibleItemPosition();
+                                    break;
+                                case MotionEvent.ACTION_MOVE:
+                                    byMotion=true;
+                                    cYy=(int)event.getY();
+                                    if(!downTwerk && cYy<=iYy && cYy>= ufYy){
+                                        upTwerk=true;
+                                        float frac=(iYy-cYy)/(float)maxYgap;
+                                        currTIME =Math.round(frac*DURATION);
+                                        twerkUp.start();
+                                        twerkUp.setCurrentPlayTime(currTIME);
+                                        twerkUp.cancel();
+                                    }break;
+                                case MotionEvent.ACTION_UP:
+                                    byMotion=false;
+                                    if(upTwerk){
+                                        twerkUpRev.setFloatValues(lastNimFrak,0F);
+                                        twerkUpRev.start();
+                                        twerkUpRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }break;
+                                case MotionEvent.ACTION_CANCEL:
+                                    byMotion=false;
+                                    if(upTwerk){
+                                        twerkUpRev.setFloatValues(lastNimFrak,0F);
+                                        twerkUpRev.start();
+                                        twerkUpRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }break;
+                            }
+                        }
+                    };
+                    break;
+                case 1:
+                    downTwerk=true;
+                    style=new TwerkStyle() {
+                        @Override
+                        public void OnTwerk(MotionEvent event) {
+                            switch (event.getAction() & event.getActionMasked()){
+                                case MotionEvent.ACTION_DOWN:
+                                    iYy=(int)event.getY();
+                                    dfYy=iYy+maxYgap;
+                                    center = getCentrallyVisibleItemPosition();
+                                    break;
+                                case MotionEvent.ACTION_MOVE:
+                                    byMotion=true;
+                                    cYy=(int)event.getY();
+                                    if(cYy>=iYy && cYy<= dfYy){
+                                        downTwerk=true;
+                                        float frac=(cYy-iYy)/(float)maxYgap;
+                                        currTIME =Math.round(frac*DURATION);
+                                        twerkDown.start();
+                                        twerkDown.setCurrentPlayTime(currTIME);
+                                        twerkDown.cancel();
+                                    }break;
+                                case MotionEvent.ACTION_UP:
+                                    byMotion=false;
+                                    if(downTwerk){
+                                        twerkDownRev.setFloatValues(lastNimFrak,0F);
+                                        twerkDownRev.start();
+                                        twerkDownRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }break;
+                                case MotionEvent.ACTION_CANCEL:
+                                    byMotion=false;
+                                    if(downTwerk){
+                                        twerkDownRev.setFloatValues(lastNimFrak,0F);
+                                        twerkDownRev.start();
+                                        twerkDownRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }break;
+                            }
+                        }
+                    };
+                    break;
+                case 2:
+                    upTwerk=true;
+                    downTwerk=true;
+                    style=new TwerkStyle() {
+                        @Override
+                        public void OnTwerk(MotionEvent event) {
+                            switch (event.getAction() & event.getActionMasked()){
+                                case MotionEvent.ACTION_DOWN:
+                                    iYy=(int)event.getY();
+                                    ufYy =iYy-maxYgap;
+                                    dfYy=iYy+maxYgap;
+                                    center = getCentrallyVisibleItemPosition();
+                                    break;
+                                case MotionEvent.ACTION_MOVE:
+                                    byMotion=true;
+                                    cYy=(int)event.getY();
+                                    if(!downTwerk && cYy<=iYy && cYy>= ufYy){
+                                        upTwerk=true;
+                                        float frac=(iYy-cYy)/(float)maxYgap;
+                                        currTIME =Math.round(frac*DURATION);
+                                        twerkUp.start();
+                                        twerkUp.setCurrentPlayTime(currTIME);
+                                        twerkUp.cancel();
+                                    }else if(!upTwerk && cYy>=iYy && cYy<= dfYy){
+                                        downTwerk=true;
+                                        float frac=(cYy-iYy)/(float)maxYgap;
+                                        currTIME =Math.round(frac*DURATION);
+                                        twerkDown.start();
+                                        twerkDown.setCurrentPlayTime(currTIME);
+                                        twerkDown.cancel();
+                                    }break;
+                                case MotionEvent.ACTION_UP:
+                                    byMotion=false;
+                                    if(upTwerk){
+                                        twerkUpRev.setFloatValues(lastNimFrak,0F);
+                                        twerkUpRev.start();
+                                        twerkUpRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }else if(downTwerk){
+                                        twerkDownRev.setFloatValues(lastNimFrak,0F);
+                                        twerkDownRev.start();
+                                        twerkDownRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }break;
+                                case MotionEvent.ACTION_CANCEL:
+                                    byMotion=false;
+                                    if(upTwerk){
+                                        twerkUpRev.setFloatValues(lastNimFrak,0F);
+                                        twerkUpRev.start();
+                                        twerkUpRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }else if(downTwerk){
+                                        twerkDownRev.setFloatValues(lastNimFrak,0F);
+                                        twerkDownRev.start();
+                                        twerkDownRev.setCurrentPlayTime(DURATION-currTIME);
+                                    }break;
+                            }
+                        }
+                    };
+                    break;
+            }
+        } finally {
+            trump_attrs.recycle();
+        }
+
+        post(new Runnable() {
+            @Override
+            public void run() {
+                maxYgap=dp2px(250,context);
+                rowHeight=getView(0).getLayoutParams().height;
+                stretchGap=Math.round(stretchScale*rowHeight);
+                shrinkGap=Math.round(shrinkScale*rowHeight);
+                setTwerking(true);
+            }
+        });
+    }
+
+    private int dp2px(float Xdp, Context context) {
+        return Math.round(Xdp * context.getResources().getDisplayMetrics().density);
+    }
+    private void preTwerk(){
+        /*ShrinkStretchSpan => rows above/below the center affected by the stretch/contraction*/
+        if(upTwerk){
+            twerkUp =ValueAnimator.ofFloat(0F,1F);
+            twerkUp.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation){
+                    lastNimFrak= TwerkUpAnimae(animation);
+                    requestLayout();
+                }
+            });
+            twerkUp.setDuration(DURATION);
+            twerkUp.setInterpolator(InterpolL);
+
+            twerkUpRev =ValueAnimator.ofFloat(1F,0F);
+            twerkUpRev.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation){
+                    TwerkUpAnimae(animation);
+                    requestLayout();
+                }
+            });
+            twerkUpRev.setDuration(DURATION);
+            twerkUpRev.setInterpolator(InterpolL);
+            twerkUpRev.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    if(!byMotion)upTwerk=false;
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+            upTwerk=false;
+        }
+        //============================================================
+        if(downTwerk){
+            twerkDown =ValueAnimator.ofFloat(0F,1F);
+            twerkDown.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation){
+                    lastNimFrak= TwerkDownAnimae(animation);
+                    requestLayout();
+                }
+            });
+            twerkDown.setDuration(DURATION);
+            twerkDown.setInterpolator(InterpolL);
+
+            twerkDownRev =ValueAnimator.ofFloat(1F,0F);
+            twerkDownRev.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation){
+                    TwerkDownAnimae(animation);
+                    requestLayout();
+                }
+            });
+            twerkDownRev.setDuration(DURATION);
+            twerkDownRev.setInterpolator(InterpolL);
+            twerkDownRev.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    if(!byMotion)downTwerk=false;
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+            downTwerk=false;
+        }
+    }
+    public void setTwerkInterpolator(Interpolator Interpol){
+        this.InterpolL =Interpol;
+    }
+    public void setTwerking(boolean twerking){
+        if(!twerking){
+            setOnTouchListener(null);
+            return;
+        }preTwerk();
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Twerk(event);
+                return false;
+            }
+        });
+    }
+    public void setTwerkingOffTime(int twerkOfftime){
+        DURATION=twerkOfftime;
+    }
+    private float TwerkUpAnimae(ValueAnimator animation){
+        float NimFrack=(float)animation.getAnimatedValue();
+        float unitGap=stretchGap/(float)ShrinkStretchSpan;
+            for (int expandIndx = center,stretchGapped=stretchGap; expandIndx < (center+ShrinkStretchSpan+1); expandIndx++){//center-down [expansion]
+                View row=getView(expandIndx);
+                if (row!=null){
+                    ViewGroup.LayoutParams vLp=row.getLayoutParams();
+                    float start=rowHeight;
+                    float end= rowHeight+stretchGapped;
+                    vLp.height=(int)((1-NimFrack)*start+ NimFrack*end);
+                    stretchGapped-=unitGap;
+                }
+            }for (int contractIndx = center-1,shrinkGapped=shrinkGap; contractIndx >(center-ShrinkStretchSpan-2) ; contractIndx--) {//center-up [contraction]
+                View row=getView(contractIndx);
+                if (row!=null){
+                    ViewGroup.LayoutParams vLp=row.getLayoutParams();
+                    float start=rowHeight;
+                    float end= rowHeight-shrinkGapped;
+                    vLp.height=(int)((1-NimFrack)*start+ NimFrack*end);
+                    shrinkGapped+=unitGap;
+                }
+            }return NimFrack;
+    }
+    private float TwerkDownAnimae(ValueAnimator animation){
+        float NimFrack=(float)animation.getAnimatedValue();
+        float unitGap=stretchGap/(float)ShrinkStretchSpan;
+            for (int expandIndx = center,shrinkGapped=shrinkGap; expandIndx < (center+ShrinkStretchSpan+1); expandIndx++){//center-down [expansion]
+                View row=getView(expandIndx);
+                if (row!=null){
+                    ViewGroup.LayoutParams vLp=row.getLayoutParams();
+                    float start=rowHeight;
+                    float end= rowHeight-shrinkGapped;
+                    vLp.height=(int)((1-NimFrack)*start+ NimFrack*end);
+                    shrinkGapped+=unitGap;
+                }
+            }for (int contractIndx = center-1,stretchGapped=stretchGap; contractIndx >(center-ShrinkStretchSpan-2) ; contractIndx--) {//center-up [contraction]
+                View row=getView(contractIndx);
+                if (row!=null){
+                    ViewGroup.LayoutParams vLp=row.getLayoutParams();
+                    float start=rowHeight;
+                    float end= rowHeight+stretchGapped;
+                    vLp.height=(int)((1-NimFrack)*start+ NimFrack*end);
+                    stretchGapped-=unitGap;
+                }
+            }return NimFrack;
+    }
+    private void Twerk(MotionEvent event){
+        style.OnTwerk(event);
+    }
+
+    private ValueAnimator twerkUpRev;
+    private ValueAnimator twerkUp;
+    private ValueAnimator twerkDown;
+    private ValueAnimator twerkDownRev;
+    private Interpolator InterpolL;
+    private static int rowHeight;
+    private int DURATION=500;
+    private int maxYgap;
+    private int center;
+    private int iYy;
+    private int ufYy;
+    private int dfYy;
+    private int currTIME;
+    private int cYy;
+    private int ShrinkStretchSpan;
+    private int stretchGap;
+    private int shrinkGap;
+    private float lastNimFrak;
+    private boolean upTwerk;
+    private boolean downTwerk;
+    private boolean byMotion;
+    private TwerkStyle style;
+
+    interface TwerkStyle{
+        public void OnTwerk(MotionEvent event);
+    }
+
+    private class IllegalTwerkingException extends IllegalStateException{
+        public IllegalTwerkingException(){
+            super("Illegal Twerking!!! Adapter must extend TwerkAdapter! let's make Twerking gr8 again(*_*)");
+        }
+    }
+    public static class TwerkAdapter extends RecyclerView.Adapter<TwerkAdapter.TwerkHolder>{
+        @Override
+        public int getItemCount(){
+            return 0;
+        }
+        @Override
+        public TwerkHolder onCreateViewHolder(ViewGroup parent, int ViewType) {
+           return null;
+        }
+        @Override
+        public void onBindViewHolder(TwerkHolder twerkReceiver, int position){
+
+        }
+        @Override
+        public void onViewAttachedToWindow(TwerkHolder twervae) {
+            super.onViewAttachedToWindow(twervae);
+            if(rowHeight!=0)twervae.v.getLayoutParams().height=rowHeight;
+        }
+        public class TwerkHolder extends  RecyclerView.ViewHolder {
+            View v;
+            public TwerkHolder(View v){
+                super(v);
+                this.v =v;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "@LiNKeR(>_<)~"+super.toString();
+    }
+}
